5622237ea3504b7a169d39586ed8af2efc4bb7c3
K V Shashank <148634273+SHASHANK8412@users.noreply.github.com>
2026-03-09 19:12:15 +0530

    Time: 3898 ms (39.39%) | Memory: 22.5 MB (84.85%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        MOD = 10**9 + 7
+        
+        dp0 = [[0]*(one+1) for _ in range(zero+1)]
+        dp1 = [[0]*(one+1) for _ in range(zero+1)]
+        
+        dp0[0][0] = dp1[0][0] = 1
+        
+        for z in range(zero+1):
+            for o in range(one+1):
+                
+                if z > 0:
+                    for k in range(1, min(limit, z)+1):
+                        dp0[z][o] = (dp0[z][o] + dp1[z-k][o]) % MOD
+                        
+                if o > 0:
+                    for k in range(1, min(limit, o)+1):
+                        dp1[z][o] = (dp1[z][o] + dp0[z][o-k]) % MOD
+        
+        return (dp0[zero][one] + dp1[zero][one]) % MOD
